2983df81b171de904be76f1f4ad98f9dd7dccc58
Will Funnell <will.funnell@bbc.co.uk>
2014-02-12 23:57:16 +0000

    Done with foreach

--- src/test/java/com/concurrent/features/ConcurrentHttpSiteProcessor.java
+++ src/test/java/com/concurrent/features/ConcurrentHttpSiteProcessor.java
@@ -2,27 +2,31 @@ package com.concurrent.features;
 
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.*;
+import akka.japi.JavaPartialFunction;
 import com.concurrent.gateway.SiteReader;
 import com.concurrent.gateway.SiteWriter;
 import com.concurrent.usecase.SiteProcessor;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.jsoup.nodes.Document;
+import scala.Function1;
 import scala.concurrent.Await;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
-import akka.util.Timeout;
 import scala.concurrent.duration.Duration;
+import akka.japi.Function;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 import static akka.dispatch.Futures.future;
 import static akka.dispatch.Futures.sequence;
+import static com.google.common.collect.ImmutableList.Builder;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class ConcurrentHttpSiteProcessor implements SiteProcessor {
@@ -47,25 +51,27 @@ public class ConcurrentHttpSiteProcessor implements SiteProcessor {
         ExecutorService executor = Executors.newFixedThreadPool(threadPool);
         ExecutionContext ec = ExecutionContexts.fromExecutorService(executor);
 
-        List<Future<Document>> futures = new ArrayList<>();
+        Builder<Future<Document>> builder = ImmutableList.builder();
         for (final String url : urls) {
-            System.out.println("creating futures");
-            futures.add(createFuture(ec, url));
+            builder.add(createFuture(ec, url));
         }
 
-        Future<Iterable<Document>> futuresSequence = sequence(futures, ec);
+        Future<Iterable<Document>> futuresSequence = sequence(builder.build(), ec);
+        futuresSequence.foreach(new JavaPartialFunction<Iterable<Document>, Object>() {
+            @Override
+            public Object apply(final Iterable<Document> docs, final boolean isCheck) throws Exception {
+                for (Document doc : docs) {
+                    writer.write(doc);
+                }
+                return null;
+            }
+        }, ec);
 
-        Iterator<Document> documents = Collections.<Document>emptyList().iterator();
         try {
-            documents = Await.result(futuresSequence, Duration.create(20, SECONDS)).iterator();
+            Await.result(futuresSequence, Duration.create(20, SECONDS));
         } catch (Exception e) {
             throw new RuntimeException();
         }
-
-        while (documents.hasNext()) {
-            writer.write(documents.next());
-        }
-
         executor.shutdown();
     }
 
--- src/test/java/com/concurrent/features/RunTests.java
+++ src/test/java/com/concurrent/features/RunTests.java
@@ -4,6 +4,6 @@ import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format={"pretty", "html:target/cucumber"})
+@Cucumber.Options(format={"html:target/cucumber"})
 public class RunTests {
 }
